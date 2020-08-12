d91c67c1f888f589209729fa1c27025ea07b18eb
iam-developer <turgunbaaev@gmail.com>
2020-08-12 22:13:02 +0300

    added changes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ let closeMenu = document.querySelector('.close-menu');
 
 function toggleMenu() {
     mobileMenu.classList.toggle('show-menu');
+    if(mobileMenu.classList.contains("show-menu")) {
+        document.getElementById('boody').style.position = "fixed";
+    }
+    if(!mobileMenu.classList.contains("show-menu")) {
+        document.getElementById('boody').style.position = "relative";
+    }
 }
 openBtn.onclick = toggleMenu;
 closeMenu.onclick = toggleMenu;
+
