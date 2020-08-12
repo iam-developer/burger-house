56de1440639cd45467eabc4e78eef0c48df5fef0
iam-developer <turgunbaaev@gmail.com>
2020-08-13 01:50:09 +0300

    still changes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// Mobile menu ========================>>>>>>
+
+// =============== BURGER MENU ============== 
 
 let openBtn = document.querySelector('.burger');
 let mobileMenu = document.querySelector('.mobile-menu');
@@ -16,3 +17,41 @@ function toggleMenu() {
 openBtn.onclick = toggleMenu;
 closeMenu.onclick = toggleMenu;
 
+
+// ============ ANIMATION ===================
+
+const animItems = document.querySelectorAll('.anim-item');
+const animScale = document.querySelectorAll('.anim-scale');
+
+function showAnimation() {
+    for(let i = 0; i < animItems.length; i ++) {
+        animItems[i].classList.add('active__class');    
+    }
+    for(let x = 0; x < animScale.length; x ++) {
+        animScale[x].classList.add('active__scale');    
+    }
+}
+setTimeout(showAnimation, 800);
+
+function scrollWin() {
+    window.scrollTo(0, 500);
+  }
+  scrollWin();
+
+  
+// =============== Scroll to Top ===============
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop() != 0)
+            $("#toTop").fadeIn();
+        else
+            $("#toTop").fadeOut();
+    });
+
+    $("#toTop").click(function() {
+        $('body, html').animate({
+            scrollTop:0
+        }, 800);
+    });
+});
