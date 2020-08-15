5734fbd2a45eecea1c5a2da350b4d8b700c512ca
iam-developer <turgunbaaev@gmail.com>
2020-08-15 12:45:43 +0300

    Added animation

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 
+// window.addEventListener('DOMContentLoaded', () => {
+
 // =============== BURGER MENU ============== 
 
 let openBtn = document.querySelector('.burger');
@@ -21,31 +23,58 @@ closeMenu.onclick = toggleMenu;
 // ============ ANIMATION ===================
 
 const animItems = document.querySelectorAll('.anim-item');
-const animScale = document.querySelectorAll('.anim-scale');
 
-function showAnimation() {
-    for(let i = 0; i < animItems.length; i ++) {
-        animItems[i].classList.add('active__class');    
+if(animItems.length > 0) {
+    window.addEventListener('scroll', showAnimation);
+    function showAnimation(params) {
+        for(let i = 0; i < animItems.length; i++) {
+            const animItem = animItems[i];          
+            const animItemHeight = animItem.offsetHeight; // Высота оего объекта
+            const animItemOffset = offset(animItem).top;  // Позиция моего объекта
+            const animStart = 4;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+            if(animItemHeight > window.innerHeight) {
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+            if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+                animItem.classList.add('active__class');    
+            }
+            else {
+                if(!animItem.classList.contains('no-anim')) {
+                    animItem.classList.remove('active__class');    
+                }
+            }
+        }
     }
-    for(let x = 0; x < animScale.length; x ++) {
-        animScale[x].classList.add('active__scale');    
+    function offset(el) {
+        const rect = el.getBoundingClientRect(),
+              scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+              scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {
+            top: rect.top + scrollTop, left: rect.left + scrollLeft
+        }
     }
 }
+
 setTimeout(showAnimation, 800);
   
 // =============== Scroll to Top ===============
 
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if($(this).scrollTop() != 0)
-            $("#toTop").fadeIn();
-        else
-            $("#toTop").fadeOut();
-    });
+    $(document).ready(function() {
+        $(window).scroll(function() {
+            if($(this).scrollTop() != 0)
+                $("#toTop").fadeIn();
+            else
+                $("#toTop").fadeOut();
+        });
 
-    $("#toTop").click(function() {
-        $('body, html').animate({
-            scrollTop:0
-        }, 800);
+        $("#toTop").click(function() {
+            $('body, html').animate({
+                scrollTop:0
+            }, 800);
+        });
     });
-});
+// });
+
